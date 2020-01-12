db5b40438160e9c2384cc7a791ae5510bd1c1b11
Kwan <ho.yi.kwan@sap.com>
2020-01-11 23:35:13 -0800

    hoyi

---
+++ src/Components/Calendar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Calendar extends Component {
+  
+    render() {
+      console.log("Calendar")
+      return(<div className="blue">
+        <h1 >Calendar</h1>
+        </div>);
+    }
+  }
+
+  export default Calendar;
